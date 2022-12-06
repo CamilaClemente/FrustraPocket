60d7c01886d482c5914b0f51d7cb5eba987005fc
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2022-12-06 17:39:33 -0300

    Delete Example directory

--- Example/job.1a0i_ATP/A/frustrar.R
+++
@@ -1,3 +0,0 @@
-library(frustratometeR)
-OrderList <- c("1a0i.pdb")
-Pdb_mut <- dir_frustration(PdbsDir = "/home/maria/Documentos/FrustraPocket/job.1a0i/",Chain = "A", OrderList = OrderList, Mode = "mutational", ResultsDir = "/home/maria/Documentos/FrustraPocket/job.1a0i/A/")
